ea88c7f7b5567b37eaf7c2a475374afd668dcb6e
ferdiamg <ferdi.armagan@synaos.com>
2020-08-24 22:36:03 +0200

    V0.2 Switched to Google Sheets

--- README.md
+++ README.md
@@ -1,27 +1,29 @@
-# eBay-Kleinanzeigen Wohnungssuche-Bot (mit SMS Benachrichtigung, .csv Tabelle, Node.js)
+# eBay-Kleinanzeigen Wohnungssuche-Bot (mit SMS Benachrichtigung, Google Sheets, Node.js)
 
 Scraped eBay-Kleinanzeigen alle 10 Minuten nach Wohnungen, die deinen Kriterien entsprechen und sendet dir eine SMS um schnell agieren zu können.
 
 ## Beschreibung
 
-Wohnungen finden im Jahre 2020 kann eine ganz schön nervenaufreibende Aufgabe sein. Durch Corona ist das Angebot schmal und die Nachfrage trotzdem stetig steigend. Da binnen weniger Minuten Inserate mit hunderten Aufrufen und Nachrichten bombardiert werden, brauchte ich eine direktere und feingranular einstellbare Möglichkeit benachrichtigt zu werden und den Überblick zu behalten.
+Wohnungen finden im Jahre 2020 kann eine ganz schön nervenaufreibende Aufgabe sein. Durch Corona ist das Angebot schmal und die Nachfrage trotzdem stetig steigend. Da binnen weniger Minuten Inserate mit hunderten Aufrufen und Nachrichten bombardiert werden, brauchte ich eine direktere und feingranular einstellbare Möglichkeit benachrichtigt zu werden und den Überblick zu behalten (Google Sheets).
 
 ## Los legen
 
 ### Dependencies
 
 * npm, node.js
-* cheerio, Twilio, node-env, node-fetch
+* cheerio, Twilio, node-env, node-fetch, GoogleSheets
 
 ### Programm ausführen
 
 * Projekt klonen: `git clone`
-* .env mit Daten befüllen
+* .env mit Daten befüllen (Google Sheets, Twilio, Kleinanzeigen-Link)
 * `npm install`
 * `npm run`
 
 ## Version History
 
+* 0.2
+    * Von .csv auf Google Sheets gewechselt
 * 0.1
     * Cronjob
     * Siehe [commits]() oder [release history]()
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const SEND_SMS = false
-
-const Apartment = require('./apartment')
 require('dotenv').config()
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
-const fs = require('fs')
-const csv = require('csv-parser');
 var CronJob = require('cron').CronJob
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+
+// Google Sheets settings
+const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_SPREADSHEET_ID)
+async function accessSpreadsheet() {
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY
+    })
+
+    await doc.loadInfo()
+    global.sheet = doc.sheetsByIndex[0]
+}
+accessSpreadsheet()
+
+// App settings
+const kleinanzeigenQueryLink = process.env.KLEINANZEIGEN_LINK
+const SEND_SMS = false
 
 // Twilio settings
 const accountSid = process.env.TWILIO_ACCOUNT_SID
@@ -16,96 +29,83 @@ const numberToSendTo = process.env.NUMBER_TO_SEND_TO
 var twilio = require('twilio')
 var client = new twilio(accountSid, authToken)
 
-let apartments = [];
-const filePath = './wohnungen.csv';
-fs.access(filePath, fs.F_OK, (err) => {
-    if(err) {
-        console.log(err)
-        // CSV headers
-        writeStream = fs.createWriteStream('wohnungen.csv')
-        writeStream.write(`Überschrift,Link,Beschreibung,Größe,Zimmer,Preis,Datum\n`)
-        return
-    }
-
-    fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('error', () => {
-            console.log("Fehler beim Einlesen der wohnungen.csv")
-        })
-        .on('data', (row) => {
-            let apartment = new Apartment(row.Überschrift, row.Link, row.Beschreibung, row.Größe, row.Zimmer, row.Preis, row.Datum)
-            apartments.push(apartment)
-        })
-        .on('end', () => {
-        })
-})
-
-var job = new CronJob('*/5 * * * * *', function() {
-  console.log('Suche nach neuen Wohnungen..')
-  fetchApartments()
+var job = new CronJob('*/5 * * * * *', function () {
+    console.log('Suche nach neuen Wohnungen..')
+    fetchApartments()
 }, null, true, 'Europe/Berlin')
 job.start()
 
-const kleinanzeigenQueryLink = process.env.KLEINANZEIGEN_LINK
-
-const fetchApartments = () => {
+async function fetchApartments() {
     fetch(kleinanzeigenQueryLink)
-    .then((res) => res.text())
-    .then((body) => {
-        const $ = cheerio.load(body)
+        .then((res) => res.text())
+        .then((body) => {
+            const $ = cheerio.load(body)
 
-        $('.ad-listitem').each((i, el) => {
-            const title = $(el).find('.text-module-begin').text().replace(/\s\s+/g, '').replace(/\,/g, ' ')
-            const link = 'https://www.ebay-kleinanzeigen.de/' + $(el).find('.text-module-begin').find('a').attr('href')
-            const description = $(el).find('.text-module-begin').next().text().replace(/\r?\n|\r/g, '').replace(/\,/g, ' ')
-            const rooms = $(el).find('.text-module-begin').next().next().children().last().text().replace(/\s\s+/g, '')
-            const price = $(el).find('.aditem-details').children().first().text().replace(/\s\s+/g, '')
-            const uploadDate = $(el).find('.aditem-details').next().text().replace(/\r?\n|\r/g, '').replace(/\s\s+/g, '').replace(/\,/, '')
-            var size = $(el).find('.text-module-begin').next().next().children().first().text().replace(/\s\s+/g, '')
-            if(size.includes(',')) size = size.substring(0, size.indexOf(','))
-            if(size.includes(' ')) size = size.substring(0, size.indexOf(' '))
+            $('.ad-listitem').each((i, el) => {
+                const title = $(el).find('.text-module-begin').text().replace(/\s\s+/g, '').replace(/\,/g, ' ')
+                const link = 'https://www.ebay-kleinanzeigen.de/' + $(el).find('.text-module-begin').find('a').attr('href')
+                const description = $(el).find('.text-module-begin').next().text().replace(/\r?\n|\r/g, '').replace(/\,/g, ' ')
+                const rooms = $(el).find('.text-module-begin').next().next().children().last().text().replace(/\s\s+/g, '')
+                const price = $(el).find('.aditem-details').children().first().text().replace(/\s\s+/g, '')
+                const uploadDate = $(el).find('.aditem-details').next().text().replace(/\r?\n|\r/g, '').replace(/\s\s+/g, '').replace(/\,/, '')
+                var size = $(el).find('.text-module-begin').next().next().children().first().text().replace(/\s\s+/g, '')
+                if (size.includes(',')) size = size.substring(0, size.indexOf(','))
+                if (size.includes(' ')) size = size.substring(0, size.indexOf(' '))
 
-            // TODO: checken ob link schon im apartments array ist, dann nicht appenden und keine SMS senden
-            if(uploadDate.toLowerCase().includes('gestern')) {
-                console.log(
-                    'Überschrift: '+title+ '\n' +
-                    'Link: '+link+  '\n' +
-                    'Beschreibung: '+description+ '\n' +
-                    'Größe: '+size+  '\n' +
-                    'Zimmer: '+rooms+ '\n' +
-                    'Preis: '+price+ '\n' +
-                    'Datum: '+uploadDate
-                 )
-                console.log('________________________')
-                
-                // Write row to CSV
-                writeStream = fs.createWriteStream('wohnungen.csv', {flags: 'a'})
-                writeStream.write(`${title},${link},${description},${size+'m²'},${rooms},${price},${uploadDate}\n`)
+                // TODO: checken ob link schon im apartments array ist, dann nicht appenden und keine SMS senden
+                if (uploadDate.toLowerCase().includes('heute')) {
+                    sheet.getRows({ query: `link = ${link}` }).then(res => {
+                        if (Object.keys(res).length === 0) {
+                            console.log(
+                                'Überschrift: ' + title + '\n' +
+                                'Link: ' + link + '\n' +
+                                'Beschreibung: ' + description + '\n' +
+                                'Größe: ' + size + '\n' +
+                                'Zimmer: ' + rooms + '\n' +
+                                'Preis: ' + price + '\n' +
+                                'Datum: ' + uploadDate
+                            )
+                            console.log('________________________')
 
-                let apartment = new Apartment(title, link, description, size, rooms, price, uploadDate)
-                apartments.push(apartment)
+                            addRow(title, link, description, size, rooms, price, uploadDate)
 
-                // Send SMS
-                if(SEND_SMS) {
-                    sendMessage(
-                        title+ '\n' +
-                        link+  '\n' +
-                        description+ '\n' +
-                        size+'m²' +  '\n' +
-                        rooms+ '\n' +
-                        price+ '\n' +
-                        uploadDate
-                    )
+                            // Send SMS
+                            if (SEND_SMS) {
+                                sendMessage(
+                                    title + '\n' +
+                                    link + '\n' +
+                                    description + '\n' +
+                                    size + 'm²' + '\n' +
+                                    rooms + '\n' +
+                                    price + '\n' +
+                                    uploadDate
+                                )
+                            }
+                        }
+                    }, err => {
+                        console.log(err)
+                    })
                 }
-            }
+            })
         })
-    })
 }
 
-const sendMessage = (message) => {
+function sendMessage(message) {
     client.messages.create({
         body: message,
         to: numberToSendTo,  // Text this number
         from: numberSentFrom // From a valid Twilio number
     }).then((message) => console.log(message.sid))
+}
+
+async function addRow(title, link, description, size, rooms, price, uploadDate) {
+    await sheet.addRow({
+        Überschrift: title,
+        Link: link,
+        Beschreibung: description,
+        Größe: size,
+        Zimmer: rooms,
+        Preis: price,
+        Datum: uploadDate,
+    })
 }
